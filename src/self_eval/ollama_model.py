70ea39d81e642353a3f813a945e74ac8c7a028f9
Dean Cahill <dpcahill98@gmail.com>
2025-12-10 15:14:26 -0500

    preliminary model run on deepseek

--- src/self_eval/ollama_model.py
+++ src/self_eval/ollama_model.py
@@ -1,32 +1,22 @@
-
+# SemEval2026
+# Task 5 --- Self Evaluation Scripts
+# OLLAMA implementation of self-evaluation preliminary
 # =============================================================================|
+import argparse
 import os
-from ollama import generate
 import json
-
 from tqdm import tqdm
 
+from ollama import generate
 from pydantic import BaseModel, Field
-from argparse import Namespace
 
-# =============================================================================|
-SYSTEM_PROMPT = '''
-You are a helpful assistant.
-Given a pre-context and a target sentence,
-please provide a score out of a five point
-scale for how likely the story will have the given ending.
-You must provide your answer using JSON Schema with the following structure:
-thought, reasoning, and score (1-5).
-'''
-
-
+from prompts import nl_template, sys_prompt_one
 # =============================================================================|
 # Data Model
 class PromptResponse(BaseModel):
     thought: str
     reasoning: str
     score: int = Field(..., ge=1, le=5, description="prediction from 1-5")
-
 
 fmt = {
             "type": "json_schema",
@@ -36,14 +26,6 @@ fmt = {
                 "strict": True
             }
 }
-
-
-def template(precontext, target, ending):
-    return f'''
-        PRE-CONTEXT: {precontext}
-        TARGET SENTENCE: {target}
-        ENDING: {ending}
-    '''
 
 
 # =============================================================================|
@@ -60,8 +42,7 @@ def construct_response_function(**params) -> callable:
                         format=PromptResponse.model_json_schema(),
                         suffix=params["suffix"],
                         think=params["think"],
-                        system=params["system_prompt"],
-                        raw=True)
+                        system=params["system_prompt"])
 
     return get_response
 
@@ -72,36 +53,59 @@ def generate_responses(instances, response_fn: callable):
     """
     for inst in instances:
         r = response_fn(inst)
-        yield PromptResponse.model_validate_json(r.message.content)
+        PromptResponse.model_validate_json(r.response)
+        yield r.response
 
 
 # =============================================================================|
 # Main
 def load_data(fp: str):
     with open(fp, 'r') as f:
-        for line in f.readlines():
-            yield json.loads(line)
+        data = json.load(f)
+        for item in list(data.items()):
+            yield item
 
 
 def main(args: dict):
     """"""
-    prompts = (template(d[1]["precontext"], d[1]["sentence"], d[1]["ending"]) for x in load_data(args["fp"]))
+    prompts = (nl_template(d[1]["precontext"],
+                            d[1]["sentence"],
+                            d[1]["ending"],
+                            d[1]["homonym"],
+                            d[1]["judged_meaning"])
+               for d in load_data(args["fp"]))
+
     inference_fn = construct_response_function(**args)
 
     with open(args["out"], 'w') as o:
         for doc in tqdm(iter(generate_responses(prompts, inference_fn))):
-            json.dump(o, doc)
+            json.dump(doc, o)
             o.write("\n")
 
 
 # TODO logprobs + P(IK) head
 # TODO argparse / config implementation
+def parse_runtime_arguments(input_fp: str) -> dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model",
+                        required=True)
+    parser.add_argument("--system_prompt")
+    parser.add_argument("--think",
+                        default=False)
+    parser.add_argument("--suffix",
+                        default="")
+    parser.add_argument("-i", "--input_fp",
+                        required=True)
+    parser.add_argument("-o", "--output_fp",
+                        required=True)
+
+
 if __name__ == "__main__":
     clargs = {"model": "deepseek-r1",
-              "system_prompt": SYSTEM_PROMPT,
+              "system_prompt": sys_prompt_one(),
               "think": False,
               "suffix": "",
               "fp": "data/sample_data.json",
-              "out": "results/deepseek.self_eval.prompt1.jsonl"
+              "out": "results/deepseek.self_eval.prompt1.nl_template.run4.jsonl"
               }
     main(clargs)
