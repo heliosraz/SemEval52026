a30d47f7f025f2b4455d89b2e6be61c647e77e8d
heliosraz <heliosra3721@gmail.com>
2025-12-31 10:24:36 -0800

    simplified data loading workflow. loading using json and filtered using pandas

--- load_data.py
+++ load_data.py
@@ -5,20 +5,12 @@ import json
 from datasets import load_dataset
 
 def load_data(path):
-    with open(path) as f:
-        big_json = json.load(f)
-        data = {id:{'precontext':big_json[id]['precontext'], 
-                    'sentence':big_json[id]['sentence'], 
-                    'ending':big_json[id]['ending'],
-                    'average':big_json[id]['average'],
-                    'stdev':big_json[id]['stdev'], 
-                    'example_sentence':big_json['example_sentence']
-                    } for id in big_json}
-    return data
-
-def make_dataset(path):
-    data = load_dataset("json", data_dir = path)
-    data['context'] = " ".join([data['precontext'],
+    with open(path, "r") as f:
+        data = json.load(f)
+        data = pd.DataFrame({'id': {'context': " ".join([data['precontext'],
                                 data['sentence'],
-                                data['ending']])
+                                data['ending']]),
+                        'sentence':data['example_sentence'], 
+                        'average':data[id]['average']
+                        } for id in data})
     return data
