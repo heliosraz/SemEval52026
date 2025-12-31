97a258c4eba24705669609e95eef64e2378e7409
heliosraz <heliosra3721@gmail.com>
2025-12-31 10:58:31 -0800

    reverting back to jyoune's implementation

--- load_data.py
+++ load_data.py
@@ -5,12 +5,7 @@ import json
 from datasets import load_dataset
 
 def load_data(path):
-    with open(path, "r") as f:
-        data = json.load(f)
-        data = pd.DataFrame({'id': {'context': " ".join([data['precontext'],
-                                data['sentence'],
-                                data['ending']]),
-                        'sentence':data['example_sentence'], 
-                        'average':data[id]['average']
-                        } for id in data})
-    return data
+    data = pd.read_json(path).transpose()
+    data['context'] = data['precontext'] + ' ' + data['sentence'] + ' ' + data['ending']
+    new_data = data[['context', 'example_sentence', 'average', 'stdev', 'homonym']]
+    return new_data
